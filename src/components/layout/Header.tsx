6f30cf03cfe4e20a90afdf92ab9d41d7f8503f44
DNgugi <dngugi42@gmail.com>
2025-05-30 15:27:12 +0800

    edited vite config for github pages

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { Menu, X, ChevronDown } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
+// import Logo from "../common/Logo";
 import logo from "../../logo.png";
 
 const Header: React.FC = () => {
